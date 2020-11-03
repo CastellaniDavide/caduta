83734eace34bfdaf2d649177b534e2c2513e4d75
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-03 07:49:34 +0100

    Created bin/template.cpp

---
+++ bin/caduta.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file caduta.cpp
+ *
+ * @version 01.01 2020-11-3
+ *
+ * @brief https://training.olinfo.it/#/task/oii_caduta/statement
+ *
+ * @ingroup caduta
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
